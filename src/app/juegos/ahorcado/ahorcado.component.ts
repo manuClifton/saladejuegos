24ba40fc8e76db02d8123e43fedae1594a3a65ae
manuClifton <manuclifton@gmail.com>
2021-09-18 12:11:00 -0300

    add game ahorcado

--- src/app/juegos/ahorcado/ahorcado.component.ts
+++ src/app/juegos/ahorcado/ahorcado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ahorcado',
@@ -7,15 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhorcadoComponent implements OnInit {
 
-  palabras = ["casa","perro","gato","avion","canasto","clima","elefante","auto","tigre","leon","computadora","celular","telefono",
-              "escritorio","boca","cabeza","zatato","camisa","pantalon","mochila","barbijo","electricidad"]
-
+  flag = true;
+  contFallas = 0;
   palabra = '';
-  
-  palabraConGuiones = '';
-  
+  guiones = '';
+  nuevoGuion = '';
   letra = '';
+  imagen = "./../../../assets/img/Ahorcado01.png";
+  imagenes = [
+              "./../../../assets/img/Ahorcado01.png",
+              "./../../../assets/img/Ahorcado02.png",
+              "./../../../assets/img/Ahorcado03.png",
+              "./../../../assets/img/Ahorcado04.png",
+              "./../../../assets/img/Ahorcado05.png",
+              "./../../../assets/img/Ahorcado06.png"
+            ]
 
+  palabras = ["casa","perro","gato","avion","canasto","clima","elefante","auto","tigre","leon","computadora","celular","telefono",
+              "escritorio","boca","cabeza","zatato","camisa","pantalon","mochila","barbijo","electricidad"];
   
 
   constructor() { }
@@ -25,19 +35,80 @@ export class AhorcadoComponent implements OnInit {
     this.crearGuiones()
   }
 
-  calcular(){
-    //alert(this.cambiarPalabra() +"  "+  this.crearGuiones())
-    this.palabra.search(this.letra) //me da la posicion de la letra si esta, sino me da -1
+  
+  verificar(){
 
-    alert()
-
+      alert(this.palabra)
+      for (let i = 0; i < this.palabra.length; i++) { 
+        if(this.palabra[i] == this.letra){
+          this.guiones = this.replaceAt(i*2, this.letra, this.guiones);
+          this.flag = false;
+        }
+      }
     
 
+    if(this.flag){
+      this.contFallas++;
+      this.imagen = this.imagenes[this.contFallas]
+      if(this.contFallas == 5){
+   
+        setTimeout(() => {
+          this.imagen = this.imagenes[5];
+        }, 1000);
+         
+     
+       
+       
+        Swal.fire({
+          icon: 'error',
+          title: 'PERDISTE',
+          text: 'Volve a intenarlo...',
+        })
+        this.actualizarPalabrta();
+        this.crearGuiones();
+        setTimeout(() => {
+          this.imagen = "./../../../assets/img/Ahorcado01.png";
+          this.contFallas = 0;
+        }, 1500);
+
+      }
     }
+
+    if(!this.guiones.includes('_')){
+      Swal.fire({
+        icon: 'success',
+        title: 'GANASTE',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      this.imagen= this.imagenes[0];
+      this.actualizarPalabrta();
+      this.crearGuiones();
+      this.contFallas = 0;
+    }
+
+    this.flag = true;
+    this.letra = '';
+    }//
+
+
+
   actualizarPalabrta(){
     return this.palabra = this.palabras[Math.floor(Math.random()*this.palabras.length)]
   }
   crearGuiones(){
-    return  this.palabraConGuiones = this.palabra.replace(/./g, "_ ");
+    return  this.guiones = this.palabra.replace(/./g, "_ ");
   }
-}
+
+
+
+  replaceAt(index:any, character:string, palabra:string) {
+     return palabra.substr(0, index) + character + palabra.substr(index+character.length); 
+    } 
+
+  click(letra:string){
+    this.letra = letra;
+  }
+
+
+}//
